06df43177567d5b3795bc2248af991f37e663886
marshaldb <dcrackel@gmail.com>
2019-12-06 02:46:31 -0500

    checking next set of changes to slowly conver he ES4 app to React Hooks

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
+//import logo from '../assets/logo.svg';
 import './App.css';
 import Header from './header/Header';
 
@@ -7,20 +7,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        
+      </div>
     </div>
   );
 }
--- src/componets/header/Header.js
+++ src/componets/header/Header.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import './Header.css';
+//import {showEverythingDrop, } from '../common/search.js';
+
+const getDropDownData = (value) =>
+{
+    if (value.length > 2) {
+      fetch('https://marshaldb.midrealm.org/mid/' + 'dropdown.php')
+      .then(res => res.json())
+      .then((data) => {
+        //this.setState({ contacts: data })
+        alert(data);
+      })
+      .catch(console.log)
+    }
+}
+
 
 function Header() {
 
   return (
-	<logobox id='logobox' className='Logo-header' >
+	<div id='logobox' className='Logo-header' >
 		<div id='logo'></div>
 		<div id='titlebox'>
-                <input id='searchbox' type='text' 
-                    onFocus='showEverythingDrop()' 
-                    onKeyUp='generateEverythingDropDown()' />
-                <button id='search' type="submit">
-                    <smallbutton />
-                </button>
+        <input id='searchbox' type='text' 
+            //onFocus={showEverythingDrop()} 
+            //onKeyUp={useKeyPress()} 
+            onChange={e => getDropDownData(e.target.value)}
+            />
+        <div className='searchicon smallbutton' />
 				<div id="everythingdropdown"></div>
 		</div>
 	 
-        <navmenu>
-            <activereport />
-	        <authreport />
-            <loginbutton />
-        </navmenu>
+        <div id='navmenu'>
+            <div id='activereport' />
+	        <div id='authreport' />
+            <div id='loginbutton' />
+        </div>
 
-	</logobox>
+	</div>
   );
 }
 

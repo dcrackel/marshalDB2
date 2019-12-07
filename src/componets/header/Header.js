d45aa1a9da48e35b60bc0d10805d5274494d3d27
marshaldb <dcrackel@gmail.com>
2019-12-07 14:40:08 -0500

    formatting on search drop down, complete. Next to start the main body cards

--- src/componets/header/Header.js
+++ src/componets/header/Header.js
@@ -8,6 +8,31 @@ function Header() {
   const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState('');
   const [doSearch, setDoSearch] = useState(false);
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const removeURLEncoding = inStr => {
+    inStr = inStr.replace(/%20/g, ' ');
+    return inStr;
+  }
+
+  const getClassForType = type => {
+    let classN = 'personicon';
+    if (type === "1") classN = 'shireicon';
+    if (type === "2") classN = 'baronyicon';
+    console.log(type + classN);
+    return classN;
+  }
+  
+  const EverythingDropDown = props => (
+    <div id="everythingdropdown" className='everythingdropdown'>
+    {data.hits.map(item => (
+        <div key={item.id} className="locitem" >
+          <div key={item.id} className={getClassForType(item.type)}></div>
+          {removeURLEncoding(item.name)}
+          </div>
+    )) }
+    </div>
+  );
 
   const searchDropDown = (text) =>  {
     setDoSearch(true);
@@ -20,20 +45,20 @@ function Header() {
 
   useEffect(() => {
     if (query.length > 1 && doSearch && debounced){
-      console.log(`${query} ${doSearch} ${debounced}`);
       const fetchData = async () => {
         setDoSearch(false);
         const result = await axios(`https://marshaldb.midrealm.org/mid/dropdown2.php?s=${query}`,);
         setData(result.data);
      };
     fetchData();
+    setShowDropDown(true);
     }
 
     if (query.length < 2 && doSearch && debounced){
        setData({ hits: [] });
-       //setQuery('');
        setDoSearch(false);
-    }
+       setShowDropDown(false);
+    }    
 
   }, [data, debounced, doSearch, query]);
 
@@ -43,13 +68,7 @@ function Header() {
 		<div id='titlebox'>
         <input id='searchbox' type='text' value={query} onChange={e => searchDropDown(e.target.value)} />
         <div className='searchicon smallbutton' />
-				<div id="everythingdropdown">
-        {data.hits.map(item => (
-          <li key={item.id}>
-            <a href={item.id}>{item.name}</a>
-          </li>
-        ))}
-        </div>
+        {showDropDown && <EverythingDropDown></EverythingDropDown>}
 		</div>
 	 
         <div id='navmenu'>

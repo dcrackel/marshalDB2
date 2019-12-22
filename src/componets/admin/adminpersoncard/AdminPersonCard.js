41150d3bd5b81a2a78679ecc312ed70bf49e6f3d
Dwain Crackel <dcrackel@gmail.com>
2019-12-22 01:11:28 -0500

    refactor to edit admin person card, to use a loop to generate form, rather than using JSX for each form value.

--- src/componets/admin/adminpersoncard/AdminPersonCard.js
+++ src/componets/admin/adminpersoncard/AdminPersonCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import globalPersonStore from "../../common/globalstore";
 import useForm from "../../common/useForm";
-//import { stateSchema, stateValidatorSchema } from "./formhelpers/FormHelpers";
+import { stateSchema, stateValidatorSchema } from "./formhelpers/FormHelpers";
 import axios from "axios";
 import "./AdminPersonCard.css";
 
@@ -12,144 +12,6 @@ function AdminPersoncard() {
   function onSubmitForm(state) {
     alert(JSON.stringify(state, null, 2));
   }
-
-  const stateValidatorSchema = {
-    sca_first_name: {
-      required: true,
-      validator: {
-        func: value => /^[a-zA-Z]+$/.test(value),
-        error: "Invalid Name Format. (no special charaters or numbers)"
-      }
-    },
-    sca_last_name: {
-      required: true,
-      validator: {
-        func: value => /^[a-zA-Z]+$/.test(value),
-        error: "Invalid Name Format. (no special charaters or numbers)"
-      }
-    },
-    legal_first_name: {
-      required: true,
-      validator: {
-        func: value => /^[a-zA-Z]+$/.test(value),
-        error: "Invalid Name Format. (no special charaters or numbers)"
-      }
-    },
-    legal_last_name: {
-      required: true,
-      validator: {
-        func: value => /^[a-zA-Z]+$/.test(value),
-        error: "Invalid Name Format. (no special charaters or numbers)"
-      }
-    },
-    branch: {
-      required: true,
-      validator: {
-        func: value => /^[\s\S]+$/.test(value),
-        error: "Invalid first name format."
-      }
-    },
-    email: {
-      required: true,
-      validator: {
-        func: value =>
-          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-            value
-          ),
-        error: "Invalid Email format."
-      }
-    },
-    membership_num: {
-      required: true,
-      validator: {
-        func: value => /^\d+$/.test(value),
-        error: "Invalid first name format."
-      }
-    },
-    membership_exp: {
-      required: true,
-      validator: {
-        func: value =>
-          /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
-            value
-          ),
-        error: "Invalid Date format DD-MM-YYYY"
-      }
-    },
-    address1: {
-      required: true,
-      validator: {
-        func: value => /^\w+$/.test(value),
-        error: "Invalid Address format."
-      }
-    },
-    address2: {
-      required: false,
-      validator: {
-        func: value => /^\w+$/.test(value),
-        error: "Invalid Address format."
-      }
-    },
-    city: {
-      required: true,
-      validator: {
-        func: value => /^[a-zA-Z]+$/.test(value),
-        error: "Invalid City format."
-      }
-    },
-    state: {
-      required: true,
-      validator: {
-        func: value => /^[a-zA-Z]+$/.test(value),
-        error: "Invalid State format."
-      }
-    },
-    zip_code: {
-      required: true,
-      validator: {
-        func: value => /^\d+$/.test(value),
-        error: "Invalid Zip Code format."
-      }
-    },
-    phone_num: {
-      required: true,
-      validator: {
-        func: value =>
-          /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(
-            value
-          ),
-        error: "Invalid Phone Number Format. Example Format: (555) 555-5555"
-      }
-    },
-    date_of_birth: {
-      required: true,
-      validator: {
-        func: value =>
-          /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
-            value
-          ),
-        error: "Invalid date format. Example Format: MM-DD-YYYYY"
-      }
-    }
-  };
-
-  const stateSchema = {
-    sca_first_name: { value: "Vincent", error: "" },
-    sca_last_name: { value: "", error: "" },
-    legal_first_name: { value: "Vincent", error: "" },
-    legal_last_name: { value: "", error: "" },
-    branch: { value: "", error: "" },
-    email: { value: "", error: "" },
-    membership_num: { value: "", error: "" },
-    membership_exp: { value: "", error: "" },
-    address1: { value: "", error: "" },
-    address2: { value: "", error: "" },
-    city: { value: "", error: "" },
-    state: { value: "", error: "" },
-    zip_code: { value: "", error: "" },
-    phone_num: { value: "", error: "" },
-    date_of_birth: { value: "", error: "" }
-  };
 
   const { values, errors, handleOnChange, handleOnSubmit, disable } = useForm(
     stateSchema,
@@ -218,271 +80,46 @@ function AdminPersoncard() {
     </div>
   );
 
-  const EditModeNode = props => (
-    <div className="personcard2">
-      <form className="innerpersoncard" onSubmit={handleOnSubmit}>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="sca_first_name">
-                SCA First Name:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="sca_first_name"
-                  value={sca_first_name}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.sca_first_name && (
-                <div className="error">{errors.sca_first_name}</div>
-              )}
-            </div>
-          </div>
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="sca_last_name">
-                SCA Last Name:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="sca_last_name"
-                  value={sca_last_name}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.sca_last_name && (
-                <div className="error">{errors.sca_last_name}</div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="legal_first_name">
-                Legal First Name:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="legal_first_name"
-                  value={legal_first_name}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.legal_first_name && (
-                <div className="error">{errors.legal_first_name}</div>
-              )}
-            </div>
-          </div>
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="legal_last_name">
-                Legal Last name:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="legal_last_name"
-                  value={legal_last_name}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.legal_last_name && (
-                <div className="error">{errors.legal_last_name}</div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="branch">
-                Branch:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="branch"
-                  value={branch}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.branch && <div className="error">{errors.branch}</div>}
-            </div>
-          </div>
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="email">
-                Email:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="email"
-                  value={email}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.email && <div className="error">{errors.email}</div>}
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="membership_num">
-                SCA Membership Number:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="membership_num"
-                  value={membership_num}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.membership_num && (
-                <div className="error">{errors.membership_num}</div>
-              )}
-            </div>
-          </div>
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="membership_exp">
-                Membership Expire Date:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="membership_exp"
-                  value={membership_exp}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.membership_exp && (
-                <div className="error">{errors.membership_exp}</div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="address1">
-                Address Line 1:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="address1"
-                  value={address1}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.address1 && <div className="error">{errors.address1}</div>}
-            </div>
-          </div>
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="address2">
-                Address Line 2:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="address2"
-                  value={address2}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.address2 && <div className="error">{errors.address2}</div>}
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="city">
-                City:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="city"
-                  value={city}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.city && <div className="error">{errors.city}</div>}
-            </div>
-          </div>
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="state">
-                State:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="state"
-                  value={state}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.state && <div className="error">{errors.state}</div>}
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="zip_code">
-                Zip Code: {errors.zip_code && <span className="error">{errors.zip_code}</span>}
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="zip_code"
-                  value={zip_code}
-                  onChange={handleOnChange}
-                />
-              </label>
-            </div>
-          </div>
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="phone_num">
-                Phone Number:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="phone_num"
-                  value={phone_num}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.phone_num && <div className="error">{errors.phone_num}</div>}
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="infoheader">
-              <label htmlFor="date_of_birth">
-                Date of Birth:
-                <input
-                  className="textinputbar"
-                  type="text"
-                  name="date_of_birth"
-                  value={date_of_birth}
-                  onChange={handleOnChange}
-                />
-              </label>
-              {errors.date_of_birth && (
-                <p className="error">{errors.date_of_birth}</p>
-              )}
-            </div>
-          </div>
-        </div>
-        <input type="submit" name="submit" disabled={disable} />
-      </form>
-    </div>
-  );
+const MakeTextField = (field_name, field_value, display_text) => (
+  <div className="column">
+  <div className="infoheader">
+    <label htmlFor="{field_name}">
+      {display_text}:
+      <input
+        className="textinputbar"
+        type="text"
+        name="{field_name}"
+        value={field_value}
+        onChange={handleOnChange}
+      />
+    </label>
+    {errors.field_name && (
+      <div className="error">{errors.field_name}</div>
+    )}
+  </div>
+</div>
+);
 
+// <div className="row"> <- might need to do that every other column?
+const MakeForm = () => (
+  <div className="personcard2">
+  <form className="innerpersoncard" onSubmit={handleOnSubmit}>
+  
+  {Object.keys(stateSchema).map((k, i) => {
+    const v = stateSchema[k].value;
+    const d = stateSchema[k].display_text;
+    return MakeTextField(k, v, d);
+  })}
+  <input type="submit" name="submit" disabled={disable} />
+  </form>
+</div>
+)
+
+
+//EditModeNode
   const PersonCard = props => (
     <div className="personcard">
-      {editMode ? <EditModeNode /> : <NotEditModeNode />}
+      {editMode ? <MakeForm /> : <NotEditModeNode />} 
     </div>
   );
 
--- src/componets/admin/adminpersoncard/formhelpers/FormHelpers.js
+++ src/componets/admin/adminpersoncard/formhelpers/FormHelpers.js
@@ -1,131 +1,137 @@
-// const stateSchema = {
-//   sca_first_name: { value: "Vincent", error: "" },
-//   sca_last_name: { value: "", error: "" },
-//   legal_first_name: { value: "Vincent", error: "" },
-//   legal_last_name: { value: "", error: "" },
-//   branch: { value: "", error: "" },
-//   email: { value: "", error: "" },
-//   membership_num: { value: "", error: "" },
-//   membership_exp: { value: "", error: "" },
-//   address1: { value: "", error: "" },
-//   address2: { value: "", error: "" },
-//   city: { value: "", error: "" },
-//   state: { value: "", error: "" },
-//   zip_code: { value: "", error: "" },
-//   phone_num: { value: "", error: "" },
-//   date_of_birth: { value: "", error: "" }
-// };
+export const stateSchema = {
+  sca_first_name: { value: "Vincent", error: "", display_text: "SCA First Name" },
+  sca_last_name: { value: "", error: "", display_text: "SCA Last Name" },
+  legal_first_name: { value: "Vincent", error: "", display_text: "Legal First Name"  },
+  legal_last_name: { value: "", error: "", display_text: "Legal Last Name"  },
+  branch: { value: "", error: "", display_text: "Local Branch"  },
+  email: { value: "", error: "", display_text: "Email"  },
+  membership_num: { value: "", error: "", display_text: "Membership Number"  },
+  membership_exp: { value: "", error: "", display_text: "Member Expire"  },
+  address1: { value: "", error: "", display_text: "Address 1"  },
+  address2: { value: "", error: "", display_text: "Address 2"  },
+  city: { value: "", error: "", display_text: "City"  },
+  state: { value: "", error: "", display_text: "State"  },
+  zip_code: { value: "", error: "", display_text: "Zip Code"  },
+  phone_num: { value: "", error: "", display_text: "Phone Number"  },
+  date_of_birth: { value: "", error: "", display_text: "Date of Birth"  }
+};
 
-// const stateValidatorSchema = {
-//   sca_first_name: {
-//     required: true,
-//     validator: {
-//       func: value => /^[a-zA-Z]+$/.test(value),
-//       error: "Invalid Name Format. (no special charaters or numbers)"
-//     }
-//   },
-//   sca_last_name: {
-//     required: true,
-//     validator: {
-//       func: value => /^[a-zA-Z]+$/.test(value),
-//       error: "Invalid Name Format. (no special charaters or numbers)"
-//     }
-//   },
-//   legal_first_name: {
-//     required: true,
-//     validator: {
-//       func: value => /^[a-zA-Z]+$/.test(value),
-//       error: "Invalid Name Format. (no special charaters or numbers)"
-//     }
-//   },
-//   legal_last_name: {
-//     required: true,
-//     validator: {
-//       func: value => /^[a-zA-Z]+$/.test(value),
-//       error: "Invalid Name Format. (no special charaters or numbers)"
-//     }
-//   },
-//   branch: {
-//     required: true,
-//     validator: {
-//       func: value => /^[\s\S]+$/.test(value),
-//       error: "Invalid first name format."
-//     }
-//   },
-//   email: {
-//     required: true,
-//     validator: {
-//       func: value =>
-//         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-//           value
-//         ),
-//       error: "Invalid Email format."
-//     }
-//   },
-//   membership_num: {
-//     required: true,
-//     validator: {
-//       func: value => /^\d+$/.test(value),
-//       error: "Invalid first name format."
-//     }
-//   },
-//   membership_exp: {
-//     required: true,
-//     validator: {
-//       func: value =>
-//         /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
-//           value
-//         ),
-//       error: "Invalid Date format DD-MM-YYYY"
-//     }
-//   },
-//   address1: {
-//     required: true,
-//     validator: {
-//       func: value => /^\w+$/.test(value),
-//       error: "Invalid Address format."
-//     }
-//   },
-//   address2: {
-//     required: false,
-//     validator: {
-//       func: value => /^\w+$/.test(value),
-//       error: "Invalid Address format."
-//     }
-//   },
-//   city: {
-//     required: true,
-//     validator: {
-//       func: value => /^[a-zA-Z]+$/.test(value),
-//       error: "Invalid City format."
-//     }
-//   },
-//   state: {
-//     required: true,
-//     validator: {
-//       func: value => /^[a-zA-Z]+$/.test(value),
-//       error: "Invalid State format."
-//     }
-//   },
-//   zip_code: {
-//     required: true,
-//     validator: {
-//       func: value => /^\d+$/.test(value),
-//       error: "Invalid Zip Code format."
-//     }
-//   },
-//   phone_num: {
-//     required: true,
-//     validator: {
-//       func: value => /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(value),
-//       error: "Invalid Phone Number Format. Example Format: (555) 555-5555"
-//     }
-//   },
-//   date_of_birth: {
-//     required: true,
-//     validator: {
-//       func: value => /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(value),
-//       error: "Invalid date format. Example Format: MM-DD-YYYYY"
-//     }
-//   }
-// };
+export const stateValidatorSchema = {
+    sca_first_name: {
+      required: true,
+      validator: {
+        func: value => /^[a-zA-Z]+$/.test(value),
+        error: "Invalid Name Format. (no special charaters or numbers)"
+      }
+    },
+    sca_last_name: {
+      required: true,
+      validator: {
+        func: value => /^[a-zA-Z]+$/.test(value),
+        error: "Invalid Name Format. (no special charaters or numbers)"
+      }
+    },
+    legal_first_name: {
+      required: true,
+      validator: {
+        func: value => /^[a-zA-Z]+$/.test(value),
+        error: "Invalid Name Format. (no special charaters or numbers)"
+      }
+    },
+    legal_last_name: {
+      required: true,
+      validator: {
+        func: value => /^[a-zA-Z]+$/.test(value),
+        error: "Invalid Name Format. (no special charaters or numbers)"
+      }
+    },
+    branch: {
+      required: true,
+      validator: {
+        func: value => /^[\s\S]+$/.test(value),
+        error: "Invalid first name format."
+      }
+    },
+    email: {
+      required: true,
+      validator: {
+        func: value =>
+          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            value
+          ),
+        error: "Invalid Email format."
+      }
+    },
+    membership_num: {
+      required: true,
+      validator: {
+        func: value => /^\d+$/.test(value),
+        error: "Invalid first name format."
+      }
+    },
+    membership_exp: {
+      required: true,
+      validator: {
+        func: value =>
+          /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
+            value
+          ),
+        error: "Invalid Date format DD-MM-YYYY"
+      }
+    },
+    address1: {
+      required: true,
+      validator: {
+        func: value => /^\w+$/.test(value),
+        error: "Invalid Address format."
+      }
+    },
+    address2: {
+      required: false,
+      validator: {
+        func: value => /^\w+$/.test(value),
+        error: "Invalid Address format."
+      }
+    },
+    city: {
+      required: true,
+      validator: {
+        func: value => /^[a-zA-Z]+$/.test(value),
+        error: "Invalid City format."
+      }
+    },
+    state: {
+      required: true,
+      validator: {
+        func: value => /^[a-zA-Z]+$/.test(value),
+        error: "Invalid State format."
+      }
+    },
+    zip_code: {
+      required: true,
+      validator: {
+        func: value => /^\d+$/.test(value),
+        error: "Invalid Zip Code format."
+      }
+    },
+    phone_num: {
+      required: true,
+      validator: {
+        func: value =>
+          /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(
+            value
+          ),
+        error: "Invalid Phone Number Format. Example Format: (555) 555-5555"
+      }
+    },
+    date_of_birth: {
+      required: true,
+      validator: {
+        func: value =>
+          /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
+            value
+          ),
+        error: "Invalid date format. Example Format: MM-DD-YYYYY"
+      }
+    }
+  };
